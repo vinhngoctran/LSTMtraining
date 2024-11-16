a3094381571512223de5e3960827cc5be6b7541a
vinhngoctran <ngocvinh.hus@gmail.com>
2024-11-16 12:28:33 -0500

    Up

--- src/plot_heatscater.m
+++ src/plot_heatscater.m
@@ -1,9 +1,11 @@
-function plot_heatscater(x,y,nbins,cb,LIMS)
+function plot_heatscater(x,y,nbins,cb,LIMS,VarName)
 % Generate sample data
 % n = 1000;
 % x = randn(n, 1);
 % y = randn(n, 1);
-% 
+if nargin < 6
+    VarName = 'NA';
+end
 % % Calculate 2D histogram
 % nbins = 50;
 [N, edges_x, edges_y] = histcounts2(x, y, nbins);
@@ -16,7 +18,7 @@ dy = diff(edges_y(1:2));
 area = dx * dy;
 density = N' / area;
 density_interp = interp2(X, Y, density, x, y, 'linear');
-scatter(x, y, 5, density_interp, 'filled');
+scatter(x, y, 5, density_interp, 'filled','DisplayName',VarName);
 % cmap = flipud(parula); % Invert parula colormap
 colormap("parula");
 if cb == 1
